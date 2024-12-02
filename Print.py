f76ee35cd15bb5546c04673d11a7220c0ada4d63
Đinh Việt Hoàng <viethoang03.work@gmail.com>
2024-12-02 12:45:41 +0700

    Create Print.py

---
+++ Print.py
@@ -0,0 +1,16 @@
+"""
+Giới thiệu câu lệnh print
+"""
+print()
+print(5)
+
+print("Hello word")
+
+print(6); print("Hello word")
+
+print("Xin","Chào", "Hoàng",sep=",") #Sep tạo ngăn cách
+
+print("Xin", "Chào", end=":") #Không xuống dòng
+print("Hoàng")
+
+print('Ten = {0}, Họ = {1}'.format('Hoàng','Đinh'))
